48c5ed4626dd8e1dd4ea649b5c689b7602925466
majooody50700-blip <majooody50700@gmail.com>
2026-09-25 17:59:42 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+const TOKEN = process.env.DISCORD_TOKEN;
+const CHANNEL_ID =  "https://discord.com/channels/1534480234349592597/1544756785460551680"; 
+const GUILD_ID = "1534480234349592597";
+
+client.once('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    try {
+        const guild = await client.guilds.fetch(GUILD_ID);
+        const channel = await guild.channels.fetch(CHANNEL_ID);
+
+        if (channel && channel.isVoiceBased()) {
+            joinVoiceChannel({
+                channelId: channel.id,
+                guildId: guild.id,
+                adapterCreator: guild.voiceAdapterCreator,
+                selfDeaf: false,
+                selfMute: false
+            });
+            console.log(`Successfully joined voice channel: ${channel.name}`);
+        } else {
+            console.log("Voice channel not found or invalid!");
+        }
+    } catch (error) {
+        console.error("Error joining voice channel:", error);
+    }
+});
+
+client.login(TOKEN);
